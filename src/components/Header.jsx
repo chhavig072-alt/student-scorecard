63559d8bc15ba533975f19f1bd4bb2c95e9902f3
Chhavi <chhavig072@gmail.com>
2026-04-05 23:33:08 +0530

    Add Header component for student scoreboard

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Student Scoreboard</h1>
+      <p>Manage student scores with dynamic updates</p>
+    </header>
+  );
+}
+
+export default Header;
